92620a49bff8f411d46b9dc0ab31f88625a2e36c
Sagar pallai <sagarpallai1997@gmail.com>
2024-03-14 12:20:58 +0530

    new file added

---
+++ Lecture 4/uber_reco/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='it will recomend prise to near by perosn as per distance',
+    author='sagar',
+    license='MIT',
+)
